5828a1c250f156ec2afd2eaa2c089876a097e17c
chandinipalem <palemc99@gmail.com>
2025-06-24 23:44:27 -0400

    edits

--- my-app/metro.config.js
+++ my-app/metro.config.js
@@ -1,18 +1,4 @@
 const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
-
-// Fix for GitHub Pages static asset path
-config.server = {
-  ...config.server,
-  enhanceMiddleware: (middleware) => {
-    return (req, res, next) => {
-      if (req.url.startsWith("/_expo/")) {
-        req.url = `/TeluguWordle${req.url}`;
-      }
-      return middleware(req, res, next);
-    };
-  },
-};
-
 module.exports = config;
